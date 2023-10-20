3bccc4bf3de5a95e33970b087325fb9c8676c0b1
rahmanrizaluddin <114675284+rahmanrizaluddin@users.noreply.github.com>
2023-10-20 10:15:51 +0700

    Add files via upload

---
+++ array.js
@@ -0,0 +1,15 @@
+const restaurant = [
+    {
+      name: "Sono Space",
+      city: "Sukabumi",
+      favoriteDrink: "Milo Cheese",
+      favoriteFood: "Chicken Katsu",
+      isVegan: true,
+    },
+  ];
+  
+  console.log("Nama : " + restaurant[0].name);
+  console.log("Kota : " + restaurant[0].city);
+  console.log("Minuman Favorit : " + restaurant[0].favoriteDrink);
+  console.log("Makanan Favorit : " + restaurant[0].favoriteFood);
+  console.log("Vegan : " + restaurant[0].isVegan);
---
+++ var.js
@@ -0,0 +1,14 @@
+const restaurant = {
+    name: "Sono Space",
+    city: "Sukabumi",
+    favoriteDrink: "Milo Cheese",
+    favoriteFood: "Chicken Katsu",
+    isVegan: false,
+  };
+  
+  const { name, city, favoriteDrink, favoriteFood, isVegan } = restaurant;
+  console.log("Nama : " + name);
+  console.log("Kota : " + city);
+  console.log("Minuman Favorit : " + favoriteDrink);
+  console.log("Makanan Favorit : " + favoriteFood);
+  console.log("Vegan : " + isVegan);
